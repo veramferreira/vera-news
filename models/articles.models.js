8689897ac7295b057487ed8515fcd9498daeebaf
Vera Ferreira <verammferreira@gmail.com>
2023-05-11 16:17:56 +0100

    apply suggested changes to model - ticket 6

--- models/articles.models.js
+++ models/articles.models.js
@@ -40,6 +40,9 @@ exports.selectCommentByArticleId = (
       [article_id]
     )
     .then((result) => {
+      if(result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "article not found!" });
+      }
       return result.rows;
     });
 };
