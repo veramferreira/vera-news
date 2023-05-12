eb635c2a491006a08072029e2acc539a8bf84cea
Vera Ferreira <verammferreira@gmail.com>
2023-05-12 10:49:07 +0100

    setup server

--- listen.js
+++ listen.js
@@ -1,6 +1,5 @@
 const app = require('./app')
 
-app.listen(9090, (err) => {
-    if (err) console.log(err)
-    else console.log('listening on port 9090...')
-})
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
